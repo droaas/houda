3d77c44a8520b8a2c5ec593bdc2ce65696b858db
droaas <157219796+droaas@users.noreply.github.com>
2024-01-20 16:18:50 +0300

    Add files via upload

---
+++ templates.py
@@ -0,0 +1,132 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+# setting explicit height and max-width: none on the SVG is required for
+# Jupyter to render it properly in a cell
+
+TPL_DEP_SVG="""<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="l11" class="displacy"
+    width="2130" height="417.0"
+    style="max-width: none; height: 417.0px; color: #000000; background: #ffffff; font-family: KFGQPC HafsEx1 Uthmanic Script"><text
+        class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="50">دِّينِ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="50">______</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="50">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em x="180">ٱل</tspan>
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em dy=".25em" x="180">__</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DarkSlateBlue" font-size=1em x="180">DET</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="310">يَوْمِ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="310">______</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="310">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="440">مَٰلِكِ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="440">_______</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="440">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DeepSkyBlue" font-size=2em x="570">رَّحِيمِ</tspan>
+        <tspan class="displacy-word" fill="DeepSkyBlue" font-size=2em dy=".25em" x="570">________</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DeepSkyBlue" font-size=1em x="570">ADJ</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em x="700">ٱل</tspan>
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em dy=".25em" x="700">__</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DarkSlateBlue" font-size=1em x="700">DET</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DeepSkyBlue" font-size=2em x="830">رَّحْمَٰنِ</tspan>
+        <tspan class="displacy-word" fill="DeepSkyBlue" font-size=2em dy=".25em" x="830">__________</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DeepSkyBlue" font-size=1em x="830">ADJ</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em x="960">ٱل</tspan>
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em dy=".25em" x="960">__</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DarkSlateBlue" font-size=1em x="960">DET</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="1090">عَٰلَمِينَ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="1090">__________</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="1090">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em x="1220">ٱلْ</tspan>
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em dy=".25em" x="1220">___</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DarkSlateBlue" font-size=1em x="1220">DET</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="1350">رَبِّ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="1350">_____</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="1350">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="LimeGreen" font-size=2em x="1480">لَّهِ</tspan>
+        <tspan class="displacy-word" fill="LimeGreen" font-size=2em dy=".25em" x="1480">_____</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="LimeGreen" font-size=1em x="1480">PN</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Red" font-size=2em x="1610">لِ</tspan>
+        <tspan class="displacy-word" fill="Red" font-size=2em dy=".25em" x="1610">__</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Red" font-size=1em x="1610">P</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="1740">(*)</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="1740">___</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="1740">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="Lime" font-size=2em x="1870">حَمْدُ</tspan>
+        <tspan class="displacy-word" fill="Lime" font-size=2em dy=".25em" x="1870">______</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="Lime" font-size=1em x="1870">N</tspan>
+    </text><text class="displacy-token" fill="currentColor" text-anchor="middle" y="357.0">
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em x="2000">ٱلْ</tspan>
+        <tspan class="displacy-word" fill="DarkSlateBlue" font-size=2em dy=".25em" x="2000">___</tspan>
+        <tspan class="displacy-tag" dy="2em" fill="DarkSlateBlue" font-size=1em x="2000">DET</tspan>
+    </text>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-0" stroke-width="2px" d="M70,327.0 C70,197.0 295.0,197.0 295.0,327.0"
+            fill="none" stroke="DodgerBlue" />
+        <path class="displacy-arrowhead" d="M174.5,228.5 v-4 l15 5 l-15 5 v-4 z " fill="DodgerBlue" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="DodgerBlue" text-anchor="middle" x="177.5" y="207.5"> مضاف إليه </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-1" stroke-width="2px" d="M330,327.0 C330,262.0 420.0,262.0 420.0,327.0"
+            fill="none" stroke="DodgerBlue" />
+        <path class="displacy-arrowhead" d="M367.0,277.25 v-4 l15 5 l-15 5 v-4 z " fill="DodgerBlue" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="DodgerBlue" text-anchor="middle" x="370.0" y="256.25"> مضاف إليه </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-2" stroke-width="2px" d="M460,327.0 C460,2.0 1480.0,2.0 1480.0,327.0"
+            fill="none" stroke="DarkGreen" />
+        <path class="displacy-arrowhead" d="M962.0,82.25 v-4 l15 5 l-15 5 v-4 z " fill="DarkGreen" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="DarkGreen" text-anchor="middle" x="965.0" y="61.25"> بدل </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-3" stroke-width="2px" d="M590,327.0 C590,67.0 1475.0,67.0 1475.0,327.0"
+            fill="none" stroke="BlueViolet" />
+        <path class="displacy-arrowhead" d="M1024.5,131.0 v-4 l15 5 l-15 5 v-4 z " fill="BlueViolet" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="BlueViolet" text-anchor="middle" x="1027.5" y="110.0"> صفة </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-4" stroke-width="2px" d="M850,327.0 C850,132.0 1470.0,132.0 1470.0,327.0"
+            fill="none" stroke="BlueViolet" />
+        <path class="displacy-arrowhead" d="M1152.0,179.75 v-4 l15 5 l-15 5 v-4 z " fill="BlueViolet" /><text
+            dy="1.25em" style="font-size: 0.8em" fill="BlueViolet" text-anchor="middle" x="1155.0" y="158.75"> صفة
+            </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-5" stroke-width="2px"
+            d="M1110,327.0 C1110,197.0 1335.0,197.0 1335.0,327.0" fill="none" stroke="DodgerBlue" />
+        <path class="displacy-arrowhead" d="M1214.5,228.5 v-4 l15 5 l-15 5 v-4 z " fill="DodgerBlue" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="DodgerBlue" text-anchor="middle" x="1217.5" y="207.5"> مضاف إليه </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-6" stroke-width="2px"
+            d="M1370,327.0 C1370,262.0 1460.0,262.0 1460.0,327.0" fill="none" stroke="DarkGreen" />
+        <path class="displacy-arrowhead" d="M1407.0,277.25 v-4 l15 5 l-15 5 v-4 z " fill="DarkGreen" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="DarkGreen" text-anchor="middle" x="1410.0" y="256.25"> بدل </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-7" stroke-width="2px"
+            d="M1500,327.0 C1500,262.0 1590.0,262.0 1590.0,327.0" fill="none" stroke="Crimson" />
+        <path class="displacy-arrowhead" d="M1537.0,277.25 v-4 l15 5 l-15 5 v-4 z " fill="Crimson" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="Crimson" text-anchor="middle" x="1540.0" y="256.25"> مجرور </textPath>
+    </g>
+    <g class="displacy-arrow">
+        <path class="displacy-arc" id="arrow-1-8" stroke-width="2px"
+            d="M1760,327.0 C1760,262.0 1850.0,262.0 1850.0,327.0" fill="none" stroke="Green" />
+        <path class="displacy-arrowhead" d="M1797.0,277.25 v-4 l15 5 l-15 5 v-4 z " fill="Green" /><text dy="1.25em"
+            style="font-size: 0.8em" fill="Green" text-anchor="middle" x="1800.0" y="256.25"> خبر </textPath>
+    </g>
+</svg>
+"""
